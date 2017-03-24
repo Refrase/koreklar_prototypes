6c594c3fd8c36705019c91b3814f90a18317c264
Andreas Rasmussen <andreasrefras@gmail.com>
2017-03-24 18:25:24 +0100

    Initial setup of environment and basic files

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+module.exports = function(grunt) {
+
+	// 1. Configure tasks
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON( 'package.json' ),
+
+    // Compile .scss-files in 'dev' folder into 1 style.css file that WP can read
+		sass: {
+			dev: {
+				options: {
+					outputStyle: 'expanded' // 'expanded' = makes the output .css-file easy to read
+				},
+				files: {
+					'style.css': 'dev/scss/style.scss'
+				}
+			},
+			build: {
+				options: {
+					outputStyle: 'compressed' // 'compressed' = minifies the output for live server
+				},
+				files: {
+					'style.css': 'dev/scss/style.scss'
+				}
+			}
+		},
+
+    // Initialize 'grunt watch'-task with livereload (also install livereload plugin in browser to make it work)
+		watch: {
+      options: { livereload: true },
+			css: {
+        files: [ 'dev/scss/**/*.scss' ],
+				tasks: [ 'sass:dev' ] // Concatenate styles on change, but don't minify while developing (see options under 'sass' ^)
+			},
+      livereload: {
+        files: [ '*.html', 'images/**/*.{png,jpg,jpeg,gif,webp,svg}' ]
+      }
+		},
+    express:{
+      all:{
+        options:{
+          port: 3000,
+          hostname: 'localhost',
+          bases: ['./'],
+          livereload: true
+        }
+      }
+    }
+
+  });
+
+  // 2. Load plugins
+  grunt.loadNpmTasks( 'grunt-contrib-watch' ); // Runs on command 'grunt watch'
+  grunt.loadNpmTasks( 'grunt-express' );
+  grunt.loadNpmTasks( 'grunt-sass' );
+
+  // 3. Register task(s)
+  grunt.registerTask( 'serve', [ 'express', 'watch' ]); // Runs on command 'grunt serve'
+  grunt.registerTask( 'uglify', [ 'sass:dev' ]); // Runs on command 'grunt uglify'
+  grunt.registerTask( 'build', [ 'sass:build' ]); // Runs on command 'grunt build'
+};
