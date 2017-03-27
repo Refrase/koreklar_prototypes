739a188c820975a427df20a4b8c85a97564240fd
Andreas Rasmussen <andreasrefras@gmail.com>
2017-03-27 10:35:27 +0200

    Installed and configured grunt-autoprefixer

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,12 +25,20 @@ module.exports = function(grunt) {
 			}
 		},
 
+    autoprefixer:{
+      dist:{
+        files:{
+          'style.css':'style.css'
+        }
+      }
+    },
+
     // Initialize 'grunt watch'-task with livereload (also install livereload plugin in browser to make it work)
 		watch: {
       options: { livereload: true },
 			css: {
         files: [ 'dev/scss/**/*.scss' ],
-				tasks: [ 'sass:dev' ] // Concatenate styles on change, but don't minify while developing (see options under 'sass' ^)
+				tasks: [ 'sass:dev', 'autoprefixer' ] // Concatenate styles on change, but don't minify while developing (see options under 'sass' ^)
 			},
       livereload: {
         files: [ '*.html', 'images/**/*.{png,jpg,jpeg,gif,webp,svg}' ]
@@ -50,12 +58,13 @@ module.exports = function(grunt) {
   });
 
   // 2. Load plugins
-  grunt.loadNpmTasks( 'grunt-contrib-watch' ); // Runs on command 'grunt watch'
   grunt.loadNpmTasks( 'grunt-express' );
   grunt.loadNpmTasks( 'grunt-sass' );
+  grunt.loadNpmTasks( 'grunt-contrib-watch' ); // Runs on command 'grunt watch'
+  grunt.loadNpmTasks( 'grunt-autoprefixer' );
 
   // 3. Register task(s)
   grunt.registerTask( 'serve', [ 'express', 'watch' ]); // Runs on command 'grunt serve'
-  grunt.registerTask( 'uglify', [ 'sass:dev' ]); // Runs on command 'grunt uglify'
-  grunt.registerTask( 'build', [ 'sass:build' ]); // Runs on command 'grunt build'
+  grunt.registerTask( 'uglify', [ 'sass:dev', 'autoprefixer' ]); // Runs on command 'grunt uglify'
+  grunt.registerTask( 'build', [ 'sass:build', 'autoprefixer' ]); // Runs on command 'grunt build'
 };
